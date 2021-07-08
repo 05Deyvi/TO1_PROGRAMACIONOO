07a7ec828540c61f2740c390e5facbc49644a224
Deyvi <Deyvi@Deyvi-PC>
2021-07-07 19:48:03 -0500

    ejercicio 1

---
+++ src/diagramasflujo/MainE1.java
@@ -0,0 +1,19 @@
+package diagramasflujo;
+
+import java.util.Scanner;
+
+public class MainE1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("ingrese su sueldo");
+		Scanner teclado =new Scanner(System.in);
+		int S = teclado.nextInt();
+		if(S<300) {
+			 System.out.println("su bonificacion es:"+ 40);
+			 
+			   }else {
+			 System.out.println("su bonifiacion es:" + 0);
+	}
+	}
+}
