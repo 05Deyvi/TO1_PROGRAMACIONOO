6120b4199ee7a57c67a22870edea0be9f252d0eb
Deyvi <Deyvi@Deyvi-PC>
2021-07-28 16:27:37 -0500

    correcion de las pruebas

---
+++ src/diagramas/de/flujo/MainCorrecion.java
@@ -0,0 +1,33 @@
+package diagramas.de.flujo;
+
+import java.util.Scanner;
+
+public class MainCorrecion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner lector = new Scanner(System.in);
+		int S = 0;
+		int P = 0;
+		int C = 0;
+
+		for (int i = 1; i != 0; i++) {
+			System.out.println("Ingrese un numero positivo");
+			int n = lector.nextInt();
+			if (n == 0) {
+				i = -1;
+
+			} else {
+				S = S + n;
+				P = S / i;
+				C = C + 1;
+
+			}
+			System.out.println("la suma es:" + S);
+			System.out.println("el promedio es:" + P);
+			System.out.println("la cantidad de numeros es:" + C);
+
+		}
+
+	}
+}
---
+++ src/diagramas/de/flujo/MainCorrecion2.java
@@ -0,0 +1,41 @@
+package diagramas.de.flujo;
+
+import java.util.Scanner;
+
+public class MainCorrecion2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner lector = new Scanner(System.in);
+		int SP = 0;
+		int SI = 0;
+		int CI = 0;
+		int PI = 0;
+	    
+
+		for (int i = 1; i != -1; i++) {
+			System.out.println("Ingrese un numero positivo");
+			int n = lector.nextInt();
+			if (n == 0) {
+				i = -2;
+
+			} else {
+				if(n%2==0) {
+				SP=SP+n;
+				
+		}   else {
+				SI= SI +n;
+				CI= CI +1;
+				PI=SI/CI;
+		}
+			}
+			System.out.println("la suma pares es:" + SP);
+			System.out.println("el promedio de impares es:" +PI );
+			System.out.println("la cantidad de numeros impares  es:" + CI);
+			
+
+		}
+
+	}
+
+}
