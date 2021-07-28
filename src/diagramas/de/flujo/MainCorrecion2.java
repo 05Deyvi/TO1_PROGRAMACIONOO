dab5b83619469b61442582809b178bb116d17af3
Deyvi <Deyvi@Deyvi-PC>
2021-07-28 16:30:17 -0500

    correcion de las pruebas

--- src/diagramas/de/flujo/MainCorrecion2.java
+++ src/diagramas/de/flujo/MainCorrecion2.java
@@ -11,7 +11,6 @@ public class MainCorrecion2 {
 		int SI = 0;
 		int CI = 0;
 		int PI = 0;
-	    
 
 		for (int i = 1; i != -1; i++) {
 			System.out.println("Ingrese un numero positivo");
@@ -20,19 +19,18 @@ public class MainCorrecion2 {
 				i = -2;
 
 			} else {
-				if(n%2==0) {
-				SP=SP+n;
-				
-		}   else {
-				SI= SI +n;
-				CI= CI +1;
-				PI=SI/CI;
-		}
+				if (n % 2 == 0) {
+					SP = SP + n;
+
+				} else {
+					SI = SI + n;
+					CI = CI + 1;
+					PI = SI / CI;
+				}
 			}
 			System.out.println("la suma pares es:" + SP);
-			System.out.println("el promedio de impares es:" +PI );
+			System.out.println("el promedio de impares es:" + PI);
 			System.out.println("la cantidad de numeros impares  es:" + CI);
-			
 
 		}
 
