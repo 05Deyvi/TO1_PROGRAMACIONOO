ee8aa4f0a2be847525c79b45bf8be679dd2427f0
Deyvi <Deyvi@192.168.1.14>
2021-08-04 20:07:26 -0500

    taller matriz de ceros

--- src/ejercicios/varios/vectores/matrices/MainMatriz2.java
+++ src/ejercicios/varios/vectores/matrices/MainMatriz2.java
@@ -6,30 +6,20 @@ public class MainMatriz2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		char A[][] = new char[5][5];
-		for (int fila = 0; fila < 3; fila++) {
-			for (int columna = 0; columna < 5; columna++) {
-				System.out.println("Ingrese valor de la fila: " + fila+"columna:" +columna);
-				// el usuario digita  el valor por teclado
-				Scanner lector = new Scanner(System.in);
-				char n = lector.next().charAt(0);
-				A[fila][columna] = 0;
-
-			}
-			
-		}
-		System.out.println(" la mtriz sera: ");
-		for (int fila = 0; fila < 5; fila++) {
-			for (int columna = 0; columna < 5; columna++) {
-				System.out.println( A[fila][columna]+" ");
+	int J[][] = new int [5][5];
+	
+	for (int fila = 0; fila <5; fila++) {
+		for (int columna = 0; columna < 5; columna++) {
+			if ( fila == columna) {
+				J[fila][columna] = 1;
 				
-			
-
+			}else{
+				J[fila][columna] = 0;
+				
 			}
-			
-			
+           System.out.print(J[fila][columna] + " ");
 		}
 		System.out.println();
 	}
-
+	}
 }
