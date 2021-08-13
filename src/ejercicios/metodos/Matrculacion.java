e2a35e2c478b1eca408b77b96b90bb2d32edf7e4
Deyvi <Deyvi@Deyvi-PC>
2021-08-13 14:10:37 -0500

    deber en parejas

--- src/ejercicios/metodos/Matrculacion.java
+++ src/ejercicios/metodos/Matrculacion.java
@@ -8,7 +8,7 @@ public class Matrculacion {
 		// TODO Auto-generated method stub
 
 	Impresora Impresion1 = new Impresora();   // instanciar la clase
-	Impresion1.Imprimir("matriculacion exitosa #123456", 12);
+	Impresion1.Imprimir("matriculacion exitosa",12);
 	
 	}
 
---
+++ src/trabajos/grupales/Cliente.java
@@ -0,0 +1,12 @@
+package trabajos.grupales;
+
+public class Cliente {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TurnoOdontologia Turno1 = new TurnoOdontologia();
+		Turno1.Imprimir("TURNO ES A  ", "DEYVI PILATAXI", "15/08/2021", "1751398254", "13:30", "123", "ANGEL COLLAGUAZO");
+		
+	}
+
+}
